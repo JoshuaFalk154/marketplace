998e3930a08eab77f4d61f5f130ae01705dfc53f
joshua579 <falkjoshua2200@gmail.com>
2024-10-16 14:14:52 +0200

    added UserService

---
+++ src/main/java/com/marketplace/marketplace/user/UserService.java
@@ -0,0 +1,18 @@
+package com.marketplace.marketplace.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Optional<User> findBySud(String sud) {
+        return userRepository.findBySud(sud);
+    }
+
+}
